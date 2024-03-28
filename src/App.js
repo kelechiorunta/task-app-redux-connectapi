55aa2cf64301015ea900fe6a8a6e5bb553a5125a
Kelechi Orunta <mac@MACs-MacBook-Pro.local>
2024-03-28 02:57:07 +0100

    Complete update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import AppLayout from './Components/AppLayout';
+import AppThemeProvider from './Components/AppThemeProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppThemeProvider>
+      <div className="App">
+        <AppLayout/>
+      </div>
+    </AppThemeProvider>
+    
   );
 }
 
