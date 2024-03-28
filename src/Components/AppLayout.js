12fd0c17a6413d64f9731d528c14d902f733628b
Kelechi Orunta <mac@MACs-MacBook-Pro.local>
2024-03-28 22:36:19 +0100

    The layout for the App

--- src/Components/AppLayout.js
+++ src/Components/AppLayout.js
@@ -6,8 +6,9 @@ import Header from './Header'
 import AppAddToList from './AppAddToList'
 import { useState } from 'react'
 import AppCompletedList from './AppCompletedList'
+import { connect } from 'react-redux'
 
-function AppLayout() {
+function AppLayout({currentStateLength}) {
     const updated_value = useSelector((state)=>state.value)
     const dispatch = useDispatch()
     const [displayedVal, setDisplayedVal] = useState(updated_value)
@@ -17,8 +18,8 @@ function AppLayout() {
     <Box p={4} width={{base:"96%", sm:'96%', md:'94%', lg:'90%'}} m='auto' shadow='md'>
         <Header/>
         <AppCompletedList />
-        <Heading p={4} w={{base: '96%', sm:'96%', md:'94%', lg:'90%'}} m='auto' textAlign={'center'}>{updated_value}</Heading>
-        <Input type='text' p={4} value={displayedVal} onChange={(e)=> setDisplayedVal(e.target.value)}/>
+        <Heading p={4} w={{base: '96%', sm:'96%', md:'94%', lg:'90%'}} m='auto' textAlign={'center'}>{`${currentStateLength} Tasks left`}</Heading>
+        <Input type='text' p={4} value={updated_value} onChange={(e)=> setDisplayedVal(e.target.value)}/>
         <Button colorScheme='blue' onClick={()=>dispatch(increment())}>Add Value</Button>
         <Button colorScheme='blue' onClick={()=>dispatch(decrement())}>Decrease Value</Button>
 
@@ -27,4 +28,9 @@ function AppLayout() {
   )
 }
 
-export default AppLayout
+const mapStateToProps = (state) =>{
+      const { tasks } = state
+      return { currentStateLength: tasks.length && tasks? tasks.length : 0 }
+}
+
+export default connect(mapStateToProps)(AppLayout)
