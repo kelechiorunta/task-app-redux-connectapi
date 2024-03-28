1707b8161a32d47b4c409ec9bfe1169537b085a9
Kelechi Orunta <mac@MACs-MacBook-Pro.local>
2024-03-28 02:45:26 +0100

    First Update, Implemented the Redux Provider Component to mount the Redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppStore from './Components/AppStore'
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={AppStore}>
+      
+        <App />
+       
+    </Provider>
   </React.StrictMode>
 );
 
