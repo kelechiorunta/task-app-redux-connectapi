a28a3c907e6eb8e294addaf7623f4deefb57ccdf
Kelechi Orunta <mac@MACs-MacBook-Pro.local>
2024-03-29 00:42:46 +0100

    Updated Completed List Component

--- src/Components/AppCompletedList.js
+++ src/Components/AppCompletedList.js
@@ -1,29 +1,37 @@
-import { ListItem, UnorderedList, Button } from '@chakra-ui/react'
+import { ListItem, UnorderedList, Button, Flex, Heading } from '@chakra-ui/react'
 import React from 'react'
 import { connect } from 'react-redux'
-import { addbytask } from '../Objects/ActionObjs'
+import { addbytask, deletebytask } from '../Objects/ActionObjs'
+import { bindActionCreators } from 'redux'
 
-function AppCompletedList({completedlist, alllist, addbytask}) {
+function AppCompletedList({completedlist, alllist, addbytask, deletebytask}) {
   return (
     <div>
-        <h1>Completed List</h1>
+        <Heading as={'h4'}>Completed List</Heading>
         <UnorderedList bg={'InfoBackground'} w={{base:'94%', sm:'96%', md:'94%', lg:'92%'}} m='auto'>
-            {(completedlist.length) && completedlist.map(i=>{
+            {(completedlist.length) && completedlist.map((i, index)=>{
                 return <ListItem shadow={'md'} bg={'blue.300'} color={'black'} border={'1px solid'} 
-                                 p={'2'} m='auto' listStyleType={'upper-alpha'} textTransform='uppercase'
-                                 textAlign={'left'} key={i.id} display={'flex'} justifyContent={'space-between'}
-                                 alignItems={'center'}>
+                            p={'2'} m='auto' listStyleType={'upper-alpha'} textTransform='capitalize'
+                            textAlign={'left'} key={index} display={'flex'} justifyContent={'space-between'}
+                            alignItems={'center'}>
                             {i.content}
-                            <Button onClick={()=>{addbytask(i); console.log(alllist)}}>Repeat Task</Button>
-                        </ListItem>})}
+                            <Flex gap={2}>
+                                <Button onClick={()=>{addbytask(i); console.log(alllist)}}>Repeat Task</Button>
+                                <Button onClick={()=>{deletebytask(i); console.log(alllist)}}>Delete Task</Button>
+                            </Flex>
+                        </ListItem>
+                    })}
         </UnorderedList>
     </div>
   )
 }
 
 const mapStateToProps = (state) =>{
-    
     return { completedlist: state.selectedtask , alllist: state.tasks }
 }
 
-export default connect(mapStateToProps, { addbytask })(AppCompletedList)
+const mapDispatchToProps = (dispatch) =>{
+    return bindActionCreators({ addbytask, deletebytask }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppCompletedList)
